a9a789db00fdc89c339deba1bfe37495f5556118
GabrielHLock <gabrielhendrick@outlook.com>
2023-03-03 09:05:23 -0500

    new file:   js/review.js

---
+++ js/review.js
@@ -0,0 +1,27 @@
+var firstName = "Gabriel"
+var lastName = "HLock"
+console.log(`First Name: ${firstName} - Last Name: ${lastName}`);
+
+var myNum = 45
+let divNum = myNum/5
+console.log(`${myNum} divided by 5 is: ${divNum}`);
+
+for(i=1;i<10;i++){
+    console.log(i);
+}
+
+for(i=10;i>0;i--){
+    console.log(i);
+}
+
+let ezpz = "For loops are easy"
+for(i=1;i<=5;i++){
+    console.log(ezpz);
+}
+
+let arr1 = [9,2,5,6]
+for (i=1;i<=1;i++){
+    console.log(`First Array Elements: ${arr1[0]} ${arr1[1]} ${arr1[2]} ${arr1[3]}`);
+}
+
+
